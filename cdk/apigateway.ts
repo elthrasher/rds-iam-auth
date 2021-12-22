0904e8cd8499a29ce94e6a938618b427e0a65c30
elthrasher <elthrasher@gmail.com>
2021-12-22 12:14:29 -0500

    update dependencies

--- cdk/apigateway.ts
+++ cdk/apigateway.ts
@@ -1,5 +1,5 @@
 import { HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2-alpha';
-import { LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { Stack } from 'aws-cdk-lib';
 
 import { LambdaFunctions } from './lambda';
@@ -8,13 +8,13 @@ export const createApiGateway = (scope: Stack, fns: LambdaFunctions): HttpApi =>
   const httpApi = new HttpApi(scope, 'IamAuthApi');
 
   httpApi.addRoutes({
-    integration: new LambdaProxyIntegration({ handler: fns.mysqlQuery }),
+    integration: new HttpLambdaIntegration('MySQLIntegration', fns.mysqlQuery),
     methods: [HttpMethod.GET],
     path: '/mysql',
   });
 
   httpApi.addRoutes({
-    integration: new LambdaProxyIntegration({ handler: fns.pgQuery }),
+    integration: new HttpLambdaIntegration('PGIntegration', fns.pgQuery),
     methods: [HttpMethod.GET],
     path: '/pg',
   });
